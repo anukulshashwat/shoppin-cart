8ba4127231eb8e1f30c38171f7f738c0e2192efb
Anukul <shashwatanukul@gmail.com>
2022-07-27 16:35:36 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import Header from "./components/Header";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Cart from "./components/Cart";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
